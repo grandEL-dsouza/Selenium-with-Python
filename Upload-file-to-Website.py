84cb31df3118fe26ec5646ada779863f71ba22dc
GrandEL <60457320+grandEL-dsouza@users.noreply.github.com>
2023-11-15 01:31:20 +0530

    Update Upload-file-to-Website.py

--- Upload-file-to-Website.py
+++ Upload-file-to-Website.py
@@ -7,12 +7,12 @@ from selenium.common.exceptions import NoSuchElementException
 import time
 import os
 
-driver = webdriver.Safari()
+driver = webdriver.Safari() #This line chooses the driver. Make sure either the driver is present in the same location and is correctly installed in the system.
 #driver = webdriver.Chrome('./chromedriver')
-driver.get("https://the-internet.herokuapp.com/upload")
+driver.get("https://the-internet.herokuapp.com/upload") #This line in selenium goes to the Website / WebApp. Here is where we define the URL we want to test
 time.sleep(5)
-upload_file = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "selenium-snapshot.png"))
-
+upload_file = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "selenium-snapshot.png")) #in case of the 'internet herokup app site' the upload file button expects an input. Hence No click action is performed here.
+#note that in my case the file that I want to upload is required to be present in the Downloads folder. You can configure the same as per your preference.
 file_input = driver.find_element(By.CSS_SELECTOR, "input[type='file']")
 file_input.send_keys(upload_file)
 driver.find_element(By.ID, "file-submit").click()
